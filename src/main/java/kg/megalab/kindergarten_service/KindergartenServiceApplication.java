93355ddf2b003530cee0ef1f8fba4f7531fda899
Aidana <aidanaabdybekova15@gmail.com>
2025-09-23 22:08:18 +0600

    Initial commit: create Spring Boot project with dependencies

---
+++ src/main/java/kg/megalab/kindergarten_service/KindergartenServiceApplication.java
@@ -0,0 +1,13 @@
+package kg.megalab.kindergarten_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindergartenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindergartenServiceApplication.class, args);
+	}
+
+}
